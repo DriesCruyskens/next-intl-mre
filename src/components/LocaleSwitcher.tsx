7e1ef9e756f6ce1a5d7e3dce3bff5b0fc30b8ba9
Dries Cruyskens <dries.cruyskens@gmail.com>
2023-08-28 19:35:58 +0200

    Switch Link to useRouter

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -2,25 +2,24 @@
 
 import { locales } from "@/i18n";
 import { useLocale } from "next-intl";
-import { usePathname } from "next-intl/client";
-import Link from "next-intl/link";
+import { usePathname, useRouter } from "next-intl/client";
 
 export default function LocaleSwitcher() {
   const pathname = usePathname();
   const currentLocale = useLocale();
+  const router = useRouter();
 
   return (
     <div className="flex flex-row gap-4">
       {locales.map((locale) => {
         return (
-          <Link
-            href={`${pathname}`}
-            locale={locale}
+          <button
+            onClick={() => router.push(pathname, { locale })}
             key={locale}
             className={`${locale == currentLocale && "underline"}`}
           >
             {locale}
-          </Link>
+          </button>
         );
       })}
     </div>
